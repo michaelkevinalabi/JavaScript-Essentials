43fc8ef9babae55eb8db06908204d75d53e68446
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-10-07 15:46:49 +0000

    created an if statement in app.js

--- myapp.js
+++ myapp.js
@@ -1,14 +1,8 @@
-function Apple( color, weight )
-{
-    this.color = color;
-    this.weight = weight;
-}
+var motorsLeft = 0,
+    motorsRight = 0,
+    greenMan = "yes";
 
-Apple.prototype = {
-    eat : function(){ return this; },
-    throw : function(){ return "throw the apple"; }
-};
-
-var apple1 = new Apple( "blue", 58 );
-var apple2 = new Apple( "black", 189 );
-var Apple3 = new Apple("white", 380 );
+if( greenMan ==="yes" )
+    {
+        console.log( 'cross the road' );
+    }
