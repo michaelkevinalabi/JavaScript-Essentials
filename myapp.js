0bba09d5db7a764bbe263151d54a2b1157edebf6
michaelkevinalabi <michael.alabi-duobu@amalitech.org>
2020-10-07 15:51:26 +0000

    created an else_if statement in app.js

--- myapp.js
+++ myapp.js
@@ -1,8 +1,12 @@
 var motorsLeft = 0,
     motorsRight = 0,
-    greenMan = "yes";
+    greenMan = "no";
 
 if( greenMan ==="yes" )
     {
         console.log( 'cross the road' );
+    }
+else if( motorsLeft ===0 )
+    {
+        console.log( 'All clear! Cross the road')
     }
